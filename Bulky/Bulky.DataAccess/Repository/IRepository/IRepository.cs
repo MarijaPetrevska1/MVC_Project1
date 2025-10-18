99a595919c4427ec48ce1d1823411076ee36c9bd
Marija Petrevska <marija_petrevska25@yahoo.com>
2025-10-18 21:25:54 +0200

    Project part 1

---
+++ Bulky/Bulky.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+namespace BulkyBook.DataAccess.Repository.IRepository
+{
+    public interface IRepository<T> where T:class
+    {
+        // Generic Repository
+        // all of the methods we want to have here
+        // T - Category
+        IEnumerable<T> GetAll(string? includeProperties = null);
+        T Get(Expression<Func<T, bool>> filter, string? includeProperties = null);
+        void Add(T entity); // we pass the objects that needs to be added
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entity);
+
+    }
+}
---
+++ Bulky/BulkyWeb/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,103 @@
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork; //вредноста на _db да се постави само еднаш
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _unitOfWork.Category.GetAll().ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Category.Add(obj);
+                _unitOfWork.Save(); //go to the database and create that category
+                TempData["success"] = "Category created successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? id) // we need the id that the user wants to edit!!!
+        {
+            if (id==null || id==0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _unitOfWork.Category.Get(u => u.Id == id); // it will find the id
+            //Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u => u.Id == id);
+            //Category? categoryFromDb2 = _db.Categories.Where(u=>u.Id==id).FirstOrDefault();
+            if(categoryFromDb==null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid) // dali se validni podatocite
+            {
+                _unitOfWork.Category.Update(obj);
+                _unitOfWork.Save();  //go to the database and create that category
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+
+        public IActionResult Delete(int? id) // we need the id that the user wants to edit!!!
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _unitOfWork.Category.Get(u => u.Id == id);  // it will find the id
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? obj = _unitOfWork.Category.Get(u => u.Id == id); 
+            if (obj==null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.Category.Remove(obj);
+            _unitOfWork.Save(); //go to the database and create that category
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
